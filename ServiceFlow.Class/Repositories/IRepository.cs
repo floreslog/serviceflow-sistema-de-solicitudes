a6a9cc6834abdd35791680a1883c67cb875729b7
Fernando Flores <luiisflores34@gmail.com>
2026-04-07 21:46:33 -0600

    feat: Creacion del IRepository generico

---
+++ ServiceFlow.Class/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using ServiceFlow.Class.Models;
+
+namespace ServiceFlow.Class.Repositories
+{
+    public interface IRepository <T> where T : IEntity
+    {
+        Task<int> Create(T IEntity);
+        Task Delete(int id);
+        Task<ICollection<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Update(T entity);
+
+    }
+}
