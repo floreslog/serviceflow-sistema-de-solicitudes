ba4562de0dba4267dc9e7519c2e7f2ed31f479f2
Fernando Flores <luiisflores34@gmail.com>
2026-04-09 22:20:42 -0600

    feat: Implementación de autenticación con Login, Registro y Logout

--- ServiceFlow.Web/Controllers/AccountController.cs
+++ ServiceFlow.Web/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ServiceFlow.Class.Models;
+using ServiceFlow.Web.ViewModels;
 
 namespace ServiceFlow.Web.Controllers
 {
@@ -13,6 +14,76 @@ namespace ServiceFlow.Web.Controllers
         {
            this.userManager = userManager;
            this.signInManager = signInManager;
+        }
+
+
+        //Login GET
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+               return View(model);
+            }
+
+            var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos.");
+                return View(model);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        //Register GET
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser
+            {
+                FirstName = model.FirstName,
+                PaternalSurname = model.PaternalSurname,
+                MaternalSurname = model.MaternalSurname,
+                Email = model.Email,
+                PhoneNumber = model.PhoneNumber,
+                UserName = model.Email
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+            if(!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return View(model);
+            }
+            TempData["Success"] = "Cuenta creada exitosamente. Inicia sesion.";
+            return RedirectToAction("Login");
+        }
+
+        //Logout
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Login");
         }
     }
 }
